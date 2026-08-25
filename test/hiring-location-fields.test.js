0395dd9815a2e7e75b407656191a7ddfc664d299
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-25 21:48:36 +0300

    test: cover remote hiring location scope

--- test/hiring-location-fields.test.js
+++ test/hiring-location-fields.test.js
@@ -1,6 +1,9 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { isHiringNonCityLocation } from '../src/hiring-location-fields.js';
+import {
+  isHiringNonCityLocation,
+  isHiringRemoteLocationScope,
+} from '../src/hiring-location-fields.js';
 
 
 test('recognises non-city hiring location groups', () => {
@@ -8,4 +11,13 @@ test('recognises non-city hiring location groups', () => {
   assert.equal(isHiringNonCityLocation('Європа'), true);
   assert.equal(isHiringNonCityLocation('EMEA'), true);
   assert.equal(isHiringNonCityLocation('Tashkent'), false);
+});
+
+test('recognises remote hiring location scopes without treating regions as remote', () => {
+  assert.equal(isHiringRemoteLocationScope('Worldwide'), true);
+  assert.equal(isHiringRemoteLocationScope('Global'), true);
+  assert.equal(isHiringRemoteLocationScope('Anywhere'), true);
+  assert.equal(isHiringRemoteLocationScope('Anywhere in Europe'), true);
+  assert.equal(isHiringRemoteLocationScope('Europe'), false);
+  assert.equal(isHiringRemoteLocationScope('EMEA'), false);
 });
