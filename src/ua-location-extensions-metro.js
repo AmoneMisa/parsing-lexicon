1aa82a311141c11ebfc80a70dc91806e3414925f
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-25 13:10:29 +0300

    fix: keep Kharkiv metro context outside station aliases

--- src/ua-location-extensions-metro.js
+++ src/ua-location-extensions-metro.js
@@ -3,7 +3,7 @@ import { locationEntries } from './location-merge.js';
 export const UA_METRO_LOCATION_EXTENSIONS = Object.freeze({
   Kharkiv: Object.freeze({
     metro: locationEntries([
-      ['Akademika Pavlova', 'Академіка Павлова', 'Академика Павлова', 'м. Академика Павлова', 'м. Ак. Павлова', 'м Ак Павлова', 'ст.м. Академіка Павлова', 'ст. м. Академіка Павлова', 'станція метро Академіка Павлова', 'станция метро Академика Павлова'],
+      ['Akademika Pavlova', 'Академіка Павлова', 'Академика Павлова', 'Ак. Павлова', 'Ак Павлова'],
     ]),
   }),
 });
