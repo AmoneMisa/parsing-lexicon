3be115e5741c51a8a2044bd626281fc5e77e836d
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 11:12:01 +0300

    refactor: keep UA metro extensions metro-only

--- src/ua-location-extensions-metro.js
+++ src/ua-location-extensions-metro.js
@@ -1,10 +1,7 @@
 import { locationEntries } from './location-merge.js';
-import { UA_SECONDARY_LOCATION_EXTENSIONS } from './ua-secondary-cities.js';
 
 export const UA_METRO_LOCATION_EXTENSIONS = Object.freeze({
-  ...UA_SECONDARY_LOCATION_EXTENSIONS,
   Kharkiv: Object.freeze({
-    ...(UA_SECONDARY_LOCATION_EXTENSIONS.Kharkiv || {}),
     metro: locationEntries([
       ['Akademika Pavlova', 'Академіка Павлова', 'Академика Павлова', 'Ак. Павлова', 'Ак Павлова'],
     ]),
