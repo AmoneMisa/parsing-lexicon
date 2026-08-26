a16e3d82242cfc9c24b299f7a35178155ceec2a2
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 11:05:28 +0300

    chore: remove unused secondary merge helper

--- src/ua-secondary-city-merge.js
+++
@@ -1,6 +0,0 @@
-import { mergeLocationCityDictionaries } from './location-merge.js';
-import { UA_SECONDARY_LOCATION_EXTENSIONS } from './ua-secondary-cities.js';
-
-export function withSecondaryUkraineLocations(base) {
-  return mergeLocationCityDictionaries(base, UA_SECONDARY_LOCATION_EXTENSIONS);
-}
