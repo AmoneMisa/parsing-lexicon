1f878302098e6d1cace21906fc630c19726479d9
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 00:33:09 +0300

    feat: parse pet-friendly housing prose

--- src/housing-features.js
+++ src/housing-features.js
@@ -9,11 +9,19 @@ export function parseHousingFeatures(value) {
     internet: null,
     courtyard: null,
     gazebo: null,
+    petsAllowed: null,
   });
 
   const internet = has(text, /(?:\bинтернет\b|\bwi[ -]?fi\b|\bроутер\b|\brouter\b|\bінтернет\b)/iu) ? true : null;
   const courtyard = has(text, /(?:\bдвор(?:ик|а|е|ом)?\b|\bвнутренн(?:ий|его)\s+двор\b|\bподворье\b|\bдвір(?:ик|і|ом)?\b|\bcourtyard\b|\byard\b)/iu) ? true : null;
   const gazebo = has(text, /(?:\bбеседк(?:а|и|е|у|ой)\b|\bальтанк(?:а|и|у|ою)\b|\bgazebo\b)/iu) ? true : null;
 
-  return deepFreeze({ internet, courtyard, gazebo });
+  let petsAllowed = null;
+  if (has(text, /(?:\bбез\s+животн\p{L}*\b|\bс\s+животн\p{L}*\s+нельзя\b|\bживотн\p{L}*\s+нельзя\b)/iu)) {
+    petsAllowed = false;
+  } else if (has(text, /(?:\bможно\s+с\s+(?:(?:небольш\p{L}*|маленьк\p{L}*|средн\p{L}*)\s+)?животн\p{L}*\b|\bс\s+животн\p{L}*\s+можно\b|\bможно\s+с\s+(?:кот\p{L}*|кошк\p{L}*|собак\p{L}*|питомц\p{L}*)\b)/iu)) {
+    petsAllowed = true;
+  }
+
+  return deepFreeze({ internet, courtyard, gazebo, petsAllowed });
 }
