0f8bd483b7dda38f06c3b8d2737c4a979d28c3ac
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 00:32:48 +0300

    feat: export housing feature parser

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ export * from './housing.js';
 export * from './housing-intent.js';
 export * from './housing-context.js';
 export * from './housing-address.js';
+export * from './housing-features.js';
 export * from './housing-structured.js';
 export * from './hiring.js';
 export * from './hiring-languages.js';
