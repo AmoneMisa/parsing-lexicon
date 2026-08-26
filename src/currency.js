d5a23e1aa35f7d8eff1d7702413d10a1beeb3267
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 07:10:48 +0300

    Expose shared currency resolution API

---
+++ src/currency.js
@@ -0,0 +1,10 @@
+export {
+  CURRENCY_SYMBOL_CANDIDATES,
+  CURRENCY_TERMS,
+} from './money-lexicon.js';
+
+export {
+  moneyCurrencyCandidatesFromText,
+  moneyCurrencyFromText,
+  moneyCurrencyPattern,
+} from './money-core.js';
