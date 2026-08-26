f95dabb5c8c3802eb0da6f24e238b5e0337a528c
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-26 07:10:44 +0300

    Type expanded currency lexicon

--- src/money-lexicon.d.ts
+++ src/money-lexicon.d.ts
@@ -1,4 +1,5 @@
 export const CURRENCY_TERMS: readonly import('../index.d.ts').LexiconEntity[];
+export const CURRENCY_SYMBOL_CANDIDATES: Readonly<Record<string, readonly string[]>>;
 export const SALARY_PERIODS: readonly import('../index.d.ts').LexiconEntity[];
 export const SALARY_MODIFIERS: Readonly<Record<string, import('../index.d.ts').LexiconEntity>>;
 export const NUMBER_MULTIPLIERS: readonly import('../index.d.ts').LexiconEntity[];
