0c8f40588af989e359ad4fb3f4f863aa9319a3cf
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 13:55:50 +0300

    Add Kharkiv 337 and 339 microdistrict aliases

---
+++ src/ua-kharkiv-microdistrict-extensions.js
@@ -0,0 +1,10 @@
+import { locationEntries } from './location-merge.js';
+
+export const UA_KHARKIV_MICRODISTRICT_EXTENSIONS = Object.freeze({
+  Kharkiv: Object.freeze({
+    microdistricts: locationEntries([
+      ['337 microdistrict', '337-й мікрорайон', '337 мікрорайон', '337-й микрорайон', '337 микрорайон', '337 мкр', '337 мкр.', '337-й мкр', '337 м/р', '337 мрн'],
+      ['339 microdistrict', '339-й мікрорайон', '339 мікрорайон', '339-й микрорайон', '339 микрорайон', '339 мкр', '339 мкр.', '339-й мкр', '339 м/р', '339 мрн'],
+    ]),
+  }),
+});
