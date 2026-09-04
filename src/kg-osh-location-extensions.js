16c3dcbd8a1e67eeaa81b22893612388846d8160
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 13:04:39 +0300

    Add Ak-Buura-3 and Mon Paris aliases for Osh

--- src/kg-osh-location-extensions.js
+++ src/kg-osh-location-extensions.js
@@ -15,9 +15,11 @@ export const KG_OSH_LOCATION_EXTENSIONS = Object.freeze({
       ['Тээке', 'Teeke', 'Тээке айылы', 'село Тээке'],
       ['Учар', 'Uchar', 'Учар айылы', 'село Учар'],
       ['Ак-Буура-2', 'Ак Буура 2', 'Ак-Бура-2', 'Ак Бура 2', 'Ak-Buura-2', 'Ak Buura 2', 'Ак-Буура-2 айылы', 'село Ак-Буура-2'],
+      ['Ак-Буура-3', 'Ак Буура 3', 'Ак-Бура-3', 'Ак Бура 3', 'Ak-Buura-3', 'Ak Buura 3', 'Ак-Буура-3 айылы', 'село Ак-Буура-3'],
     ]),
     residentialComplexes: locationEntries([
       ['Asman Residence 1', 'Asman Residence-1', 'ASMAN RESIDENCE 1', 'Асман Резиденс 1', 'Асман Резиденс-1', 'ЖК Asman Residence 1', 'ЖК Асман Резиденс 1', 'Asman Residence 1 турак жай комплекси'],
+      ['Mon Paris', 'MON PARIS', 'Mon Paris Ош', 'Mon Paris Osh', 'ЖК Mon Paris', 'ЖК Мон Париж', 'Мон Париж', 'Мон Пари', 'жилой комплекс Мон Париж', 'Mon Paris турак жай комплекси'],
     ]),
   }),
 });
