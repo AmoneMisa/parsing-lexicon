00cdba06300cfa82dd3587aec40096272c4258d0
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 08:57:40 +0300

    feat(kz): merge scrape translation layer

--- src/locations-runtime.js
+++ src/locations-runtime.js
@@ -8,6 +8,7 @@ import {
 import { LOCATION_LIST_KEYS, mergeLocationCountries } from './location-merge.js';
 import { KG_LOCATION_EXTENSIONS } from './kg-location-extensions.js';
 import { KZ_DISTRICT_EXTENSIONS } from './kz-district-extensions.js';
+import { KZ_SCRAPED_LOCATION_EXTENSIONS } from './kz-scraped-location-extensions.js';
 import { UZ_TASHKENT_CONTEXT_EXTENSIONS } from './uz-tashkent-context-extensions.js';
 
 export { UA_REGION_ENTRIES, UA_SECONDARY_CITIES, matchUkraineRegion, matchUkraineSecondaryCity };
@@ -21,6 +22,7 @@ export const LOCATION_DICTIONARIES = Object.freeze({
   KZ: mergeLocationCountries(
     BASE_LOCATION_DICTIONARIES.KZ || {},
     KZ_DISTRICT_EXTENSIONS,
+    KZ_SCRAPED_LOCATION_EXTENSIONS,
   ),
   KG: mergeLocationCountries(
     BASE_LOCATION_DICTIONARIES.KG || {},
