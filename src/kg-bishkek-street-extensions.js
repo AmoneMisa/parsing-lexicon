caaa7cc407a5eb21f98a6009b8784cabc840eb61
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 12:47:24 +0300

    Add canonical Bishkek street aliases

---
+++ src/kg-bishkek-street-extensions.js
@@ -0,0 +1,18 @@
+import { locationEntries } from './location-merge.js';
+
+export const KG_BISHKEK_STREET_EXTENSIONS = Object.freeze({
+  Bishkek: Object.freeze({
+    streets: locationEntries([
+      ['Chyngyz Aytmatov Avenue', 'Chingiz Aitmatov Avenue', 'Chingiz Aytmatov Avenue', 'проспект Чингиза Айтматова', 'проспект Чынгыза Айтматова', 'проспект Чингиза Айтматова', 'Чыңгыз Айтматов проспекти', 'Чингиз Айтматов проспекти', 'проспект Мира', 'Mira Avenue'],
+      ['Aaly Tokombayev Avenue', 'Aaly Tokombaev Avenue', 'Aaly Tokombayev Street', 'Aaly Tokombaev Street', 'проспект Аалы Токомбаева', 'улица Аалы Токомбаева', 'ул. Аалы Токомбаева', 'Аалы Токомбаев проспекти', 'Аалы Токомбаев көчөсү'],
+      ['Manas Avenue', 'Manas avenue', 'проспект Манаса', 'пр-т Манаса', 'Манас проспекти'],
+      ['Chui Avenue', 'Chuy Avenue', 'Chui avenue', 'Чуйский проспект', 'проспект Чуй', 'проспект Чүй', 'Чүй проспекти'],
+      ['Baitik Baatyr Street', 'Baitik baatyr Street', 'Baytik Baatyr Street', 'улица Байтик-Баатыра', 'улица Байтик Баатыра', 'ул. Байтик-Баатыра', 'Байтик Баатыр көчөсү'],
+      ['Nasirdin Isanov Street', 'Isanov Street', 'улица Исанова', 'улица Насирдина Исанова', 'ул. Исанова', 'Насирдин Исанов көчөсү'],
+      ['Ibraimov Street', 'Ibragimov Street', 'улица Ибраимова', 'ул. Ибраимова', 'Ибраимов көчөсү'],
+      ['Toktogul Street', 'Toktogul Satylganov Street', 'улица Токтогула', 'улица Токтогула Сатылганова', 'ул. Токтогула', 'Токтогул көчөсү', 'Токтогул Сатылганов көчөсү'],
+      ['Joomart Bokonbayev Street', 'Joomart Bokonbaev Street', 'Bokonbayev Street', 'Bokonbaev Street', 'улица Боконбаева', 'улица Жоомарта Боконбаева', 'ул. Боконбаева', 'Жоомарт Бөкөнбаев көчөсү'],
+      ['Jusup Abdrakhmanov Street', 'Yusup Abdrakhmanov Street', 'Jusup Abdrakhmanov Street', 'Abdrakhmanov Street', 'улица Юсупа Абдрахманова', 'улица Жусупа Абдрахманова', 'ул. Абдрахманова', 'Жусуп Абдрахманов көчөсү'],
+    ]),
+  }),
+});
