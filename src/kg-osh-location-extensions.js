1f10ef6a4951b9a79bb709ef0475f7133648d2df
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 12:53:56 +0300

    Add Osh settlement and residential aliases

--- src/kg-osh-location-extensions.js
+++ src/kg-osh-location-extensions.js
@@ -6,5 +6,17 @@ export const KG_OSH_LOCATION_EXTENSIONS = Object.freeze({
       ['Anar', 'Анар', 'микрорайон Анар', 'мкр Анар', 'Анар микрорайон', 'Anar microdistrict'],
       ['Tuleyken', 'Тулейкен', 'Толойкон', 'Төлөйкөн', 'микрорайон Тулейкен', 'микрорайон Толойкон', 'мкр Тулейкен', 'Tuleyken microdistrict', 'Toloikon microdistrict'],
     ]),
+    settlements: locationEntries([
+      ['Кеңеш', 'Кенеш', 'Kenesh', 'Кеңеш айылы', 'село Кеңеш', 'село Кенеш'],
+      ['Керме-Тоо', 'Керме Тоо', 'Kerme-Too', 'Kerme Too', 'Керме-Тоо айылы', 'село Керме-Тоо'],
+      ['Озгур', 'Ozgur', 'Озгур айылы', 'село Озгур'],
+      ['Орке', 'Orke', 'Орке айылы', 'село Орке'],
+      ['Пятилетка', 'Pyatiletka', 'Пятилетка айылы', 'село Пятилетка'],
+      ['Тээке', 'Teeke', 'Тээке айылы', 'село Тээке'],
+      ['Учар', 'Uchar', 'Учар айылы', 'село Учар'],
+    ]),
+    residentialComplexes: locationEntries([
+      ['Asman Residence 1', 'Asman Residence-1', 'ASMAN RESIDENCE 1', 'Асман Резиденс 1', 'Асман Резиденс-1', 'ЖК Asman Residence 1', 'ЖК Асман Резиденс 1', 'Asman Residence 1 турак жай комплекси'],
+    ]),
   }),
 });
