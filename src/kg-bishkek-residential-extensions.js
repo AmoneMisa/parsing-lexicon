41c4bd4545664a42d8948fa7307b851e6da1781d
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-04 13:14:54 +0300

    Add Bishkek residential translations

---
+++ src/kg-bishkek-residential-extensions.js
@@ -0,0 +1,16 @@
+import { locationEntries } from './location-merge.js';
+
+export const KG_BISHKEK_RESIDENTIAL_EXTENSIONS = Object.freeze({
+  Bishkek: Object.freeze({
+    residentialComplexes: locationEntries([
+      ['Senator', 'SENATOR', 'Сенатор', 'ЖК Senator', 'ЖК Сенатор', 'жилой дом Сенатор', 'Senator турак жай комплекси'],
+      ['Muras Nuru', 'MURAS NURU', 'Мурас Нуру', 'Мурас-Нуру', 'ЖК Мурас Нуру', 'Бульвар Мурас Нуру', 'ЖК Бульвар Мурас Нуру', 'Muras Nuru турак жай комплекси'],
+      ['Mansara', 'MANSARA', 'Мансара', 'ЖК Mansara', 'ЖК Мансара', 'МФК Mansara', 'многофункциональный жилой комплекс Mansara', 'Mansara турак жай комплекси'],
+      ['Regency', 'REGENCY', 'Редженси', 'Редженси', 'ЖК Regency', 'ЖД Regency', 'жилой дом Regency', 'ЖК Редженси', 'Regency турак жай комплекси'],
+      ['Илим Плюс', 'Илим плюс', 'Илим +', 'Ilim Plus', 'Ilim+', 'ЖК Илим Плюс', 'ЖД Илим Плюс', 'Илим Плюс турак жай комплекси'],
+      ['Академия', 'Akademiya', 'Academia', 'ЖК Академия', 'жилой комплекс Академия', 'Академия турак жай комплекси'],
+      ['Елисейские поля', 'Елисейские Поля', 'Eliseiskie Polya', 'Elysian Fields', 'ЖК Елисейские поля', 'Елисейские поля Авангард', 'Елисейские поля Avangard', 'Елисейские поля турак жай комплекси'],
+      ['TIANSHAN-1', 'Tianshan-1', 'TIAN SHAN-1', 'Tian Shan 1', 'Тяньшань-1', 'Тянь-Шань-1', 'ЖК TIANSHAN-1', 'ЖК Тянь-Шань-1', 'TIANSHAN-1 турак жай комплекси'],
+    ]),
+  }),
+});
