9c02260df0dd2116ddb659a25c85e1e81ef922a7
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-09-08 21:05:55 +0300

    fix: make escapeRegex root export unambiguous

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './lexicon-core.js';
 export * from './normalization.js';
+export { escapeRegex } from './normalization.js';
 export * from './parser-core.js';
 export * from './temporal.js';
 export * from './countries.js';
